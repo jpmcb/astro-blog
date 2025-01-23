830870e49cabfef4a4785e04bb0c0e236504a809
John McBirde <jpmmcbride@gmail.com>
2025-01-22 19:12:21 -0700

    feat: Full content rss feed
    Signed-off-by: John McBirde <jpmmcbride@gmail.com>

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,29 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
-	const posts = await getCollection('posts');
+    const posts = (await getCollection('posts'))
+        .filter(post => !post.data.isDraft)
+        .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/archive/${post.id}/`,
-		})),
+
+        items: posts.map((post) => ({
+          title: post.data.title,
+          pubDate: post.data.pubDate,
+          description: post.data.description,
+          link: `/archive/${post.id}/`,
+          content: sanitizeHtml(parser.render(post.body), {
+            allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+          }),
+        })),
 	});
 }
