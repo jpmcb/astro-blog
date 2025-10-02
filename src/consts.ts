e91277aecfd32ed38a44c930679ee7e1515c0c17
John McBride <jpmmcbride@gmail.com>
2025-10-02 08:27:27 -0400

    feat: Changes.
    Signed-off-by: John McBride <jpmmcbride@gmail.com>

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'John McBride';
-export const SITE_DESCRIPTION = 'John McBride\'s landing page';
+export const SITE_TITLE = 'john mcbride';
+export const SITE_DESCRIPTION = 'john mcbride\'s landing page';
